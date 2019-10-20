7ba61e17f15aa74a5acff052275a6d404b0ace11
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-10-19 21:45:18 -0400

    Added dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-mdb",
-    version="0.0.1a3",
+    version="0.0.1a5",
     author="Ryan Zembrodt",
     author_email="ryan.zembrodt@gmail.com",
     description="Package for parsing IMDb datasets and scraping web pages",
@@ -17,5 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "selectolax"
     ],
 )
