df6efaa26e9a6e2bbf5d7378ebfac687060ae33f
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-10-22 23:55:23 -0400

    Added skeleton unit tests for PyMDbScraper

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -2,3 +2,123 @@
 
 import unittest
 
+
+class TestGetTitle:
+    def test_get_title_movie(self):
+        pass
+
+    def test_get_title_tv_series(self):
+        pass
+
+    def test_get_title_tv_episode(self):
+        pass
+
+    def test_get_title_bad_request(self):
+        pass
+
+
+class TestGetFullCast:
+    def test_get_full_cast_movie(self):
+        pass
+
+    def test_get_full_cast_tv_series(self):
+        pass
+
+    def test_get_full_cast_tv_series_and_episodes(self):
+        pass
+
+    def test_get_full_cast_episode(self):
+        pass
+
+    def test_get_full_cast_bad_request(self):
+        pass
+
+
+class TestGetFullCredits:
+    def test_get_full_credits_movie(self):
+        pass
+
+    def test_get_full_credits_tv_series(self):
+        pass
+
+    def test_get_full_credits_tv_episode(self):
+        pass
+
+    def test_get_full_credits_bad_request(self):
+        pass
+
+
+class TestGetName:
+    def test_get_name_actor_alive(self):
+        pass
+
+    def test_get_name_actor_deceased(self):
+        pass
+
+    def test_get_name_director(self):
+        pass
+
+    def test_get_name_crew_member(self):
+        pass
+
+    def test_get_name_bad_request(self):
+        pass
+
+
+class TestGetNameCredits:
+    def test_get_name_actor(self):
+        pass
+
+    def test_get_name_actor_with_episodes(self):
+        pass
+
+    def test_get_name_director(self):
+        pass
+
+    def test_get_name_director_with_episodes(self):
+        pass
+
+    def test_get_name_crew_member(self):
+        pass
+
+    def test_get_name_crew_member_with_episodes(self):
+        pass
+
+    def test_get_name_bad_request(self):
+        pass
+
+
+class TestGetCompany:
+    def test_get_company(self):
+        pass
+
+    def test_get_company_bad_request(self):
+        pass
+
+
+class TestGetCompanyCredits:
+    def test_get_company_credits_movie(self):
+        pass
+
+    def test_get_company_credits_tv_series(self):
+        pass
+
+    def test_get_company_credits_tv_episode(self):
+        pass
+
+    def test_get_company_credits_bad_request(self):
+        pass
+
+
+class TestGetTechSpecs:
+    def test_get_tech_specs_movie(self):
+        pass
+
+    def test_get_tech_specs_tv_series(self):
+        pass
+
+    def test_get_tech_specs_tv_episode(self):
+        pass
+
+    def test_get_tech_specs_bad_request(self):
+        pass
