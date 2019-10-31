5e5d8dc8090c0b17c9459a710d24f7a6887ddb52
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-10-31 10:14:12 -0400

    Added skeleton unit tests for PyMDbParser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,200 @@
 """Module to test functionality of the PyMDbParser."""
 
 import unittest
+from pymdb.parser import PyMDbParser
 
+
+class TestGetTitleAkas:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestGetTitleBasics:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestGetTitleCrew:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestGetTitleEpisodes:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestGetTitlePrincipals:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestGetTitleRatings:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestGetNameBasics:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
+
+    def test_gunzip_delete_default_filenames(self):
+        pass
+
+    def test_gunzip_delete_custom_filenames(self):
+        pass
+
+    def test_contains_headers(self):
+        pass
+
+    def test_no_headers(self):
+        pass
+
+
+class TestBuildPath:
+    def test_default_filenames(self):
+        pass
+
+    def test_custom_filenames(self):
+        pass
+
+    def test_gunzip_default_filenames(self):
+        pass
+
+    def test_gunzip_custom_filenames(self):
+        pass
