bbc4f70ef1e8eba9b61f9962ddee1471f21af922
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-11-13 12:34:20 -0500

    Fixed documentation typo

--- pymdb/exceptions.py
+++ pymdb/exceptions.py
@@ -1,4 +1,4 @@
-"""Module containing exception classes unique to PyMDb"""
+"""Module containing exception classes unique to PyMDb."""
 
 class InvalidCompanyId(Exception):
     """Raised when an invalid company ID has been used in an IMDb request."""
