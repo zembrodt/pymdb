e12a4f5e597a1f8c123d8cb8ba411a86d66cb0a9
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-11-14 11:59:38 -0500

    Bump version for initial pre-release v0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Ryan Zembrodt'
 author = 'Ryan Zembrodt'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
