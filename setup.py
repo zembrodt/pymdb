4c716782ac8a9b0eed5f17d3b0456a4c3904f485
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-11-15 14:09:18 -0500

    Added supported Python versions to setup

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py-mdb",
-    version="0.1.0",
-    author="Ryan Zembrodt",
-    author_email="ryan.zembrodt@gmail.com",
-    description="Package for parsing IMDb datasets and scraping web pages",
+    name='py-mdb',
+    version='0.1.0a1',
+    author='Ryan Zembrodt',
+    author_email='ryan.zembrodt@gmail.com',
+    description='Package for parsing IMDb datasets and scraping web pages',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/zembrodt/pymdb",
+    long_description_content_type='text/markdown',
+    url='https://github.com/zembrodt/pymdb',
     packages=setuptools.find_packages(),
+    python_requires='>=3.6.0, <3.8.0',
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     keywords = ['imdb', 'pymdb', 'movie', 'film', 'tv', 'series', 'show', 'episode', 
     'database', 'db', 'dataset', 'web scrape', 'scrape', 'actor', 'actress', 'director',
     'writer', 'person', 'title', 'company', 'rating'
     ],
     install_requires=[
-        "requests",
-        "selectolax"
+        'requests',
+        'selectolax'
     ],
 )
