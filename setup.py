750ffdc1df350202f5e9cb8e22c5081a7def2dcc
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-11-29 14:41:05 -0500

    #7 Added support for Python 3.8

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/zembrodt/pymdb',
     packages=setuptools.find_packages(),
-    python_requires='>=3.6.0, <3.8.0',
+    python_requires='>=3.6.0, <3.9.0',
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
